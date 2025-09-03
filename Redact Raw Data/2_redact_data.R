41d66e85836bdf0ecbfd23418bb721afa51068f6
Jeremy Eberle <jwe4ec@virginia.edu>
2025-09-03 16:44:03 -0500

    Redact raw data

---
+++ Redact Raw Data/2_redact_data.R
@@ -0,0 +1,151 @@
+# ---------------------------------------------------------------------------- #
+# Redact Data
+# Author: Jeremy W. Eberle
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Notes ----
+# ---------------------------------------------------------------------------- #
+
+# Before running this script, restart R (CTRL+SHIFT+F10 on Windows), set your working 
+# directory to the parent folder, and ensure that the raw CSV files obtained from the 
+# Private Component of the Managing Anxiety OSF project (https://osf.io/pvd67/) are in 
+# "./data/1_raw_full/". This script will import raw data from "./data/1_raw_full/" and 
+# output redacted data to "./data/2_redacted/".
+
+# For raw data files that contain potential identifiers, this script redacts the
+# relevant data so that subsequent cleaning can be run on a dataset that can be 
+# shared and made public. This way all data cleaning steps are transparent and 
+# reproducible without compromising potentially identifiable information.
+
+# Rather than changing the structure of the raw data files, this script replaces
+# potentially identifiable values with the value "REDACTED_BY_CLEANING_SCRIPT".
+# Because it does not change the raw data structure, the script should run on 
+# already redacted data files in addition to raw data files without error.
+
+# Scope: This script is based on data dumped from the Managing Anxiety SQL server on 
+# February 2, 2019. This script may need to be updated when applied to data downloaded
+# before or after this date, as there may have been changes to the database or newly 
+# collected data not accounted for by this script.
+
+# ---------------------------------------------------------------------------- #
+# Store working directory, install correct R version, load packages ----
+# ---------------------------------------------------------------------------- #
+
+# Store working directory
+
+wd_dir <- getwd()
+
+# Load custom functions
+
+source("./Redact Raw Data/1_define_functions.R")
+
+# Check correct R version, load groundhog package, and specify groundhog_day
+
+groundhog_day <- version_control()
+
+# No packages loaded
+
+# ---------------------------------------------------------------------------- #
+# Import raw data ----
+# ---------------------------------------------------------------------------- #
+
+# Import 20 raw CSV files obtained from the Private Component of the MindTrails 
+# Managing Anxiety study's OSF project (https://osf.io/pvd67/) on 9/3/2025. Yuhan
+# Hou had dumped these files from the Managing Anxiety SQL server on 2/9/2019. She
+# used Grafana to dump all the files except for the "TrialDAO" file, which was too 
+# large to dump via Grafana and which she thus dumped from the SQL server directly.
+
+# Obtain file names of raw CSV data files
+
+raw_data_dir <- paste0(wd_dir, "/data/1_raw_full/")
+filenames <- list.files(raw_data_dir, pattern = "*.csv", full.names = FALSE)
+
+# Import data files into list
+
+dat <- lapply(paste0(raw_data_dir, filenames), read.csv)
+
+# Name data tables in list (keeping the part of the filename that comes before the 
+# first underscore that is followed by a digit)
+
+names(dat) <- sub("_\\d.*", "", filenames)
+
+# ---------------------------------------------------------------------------- #
+# Specify columns to retain ----
+# ---------------------------------------------------------------------------- #
+
+# Regarding date-related columns across tables, Bethany Teachman indicated on 
+# 1/13/2021 that MindTrails is not subject to HIPAA regulations, so restrictions 
+# on disclosing dates of training, dates of test measures, and birth years for 
+# any participants over age 89 do not apply
+
+# ---------------------------------------------------------------------------- #
+# Identify character columns ----
+# ---------------------------------------------------------------------------- #
+
+# Specify character columns to ignore
+
+ignore_cols <- c("date", "datetime", "session", "corrected_session", "sessionName")
+
+# Create list of table names whose tables contain nonignorable character columns
+
+char_cols_to_check <- lapply(dat, function(df) {
+  char_cols <- names(df)[sapply(df, is.character)]
+  
+  setdiff(char_cols, ignore_cols)
+})
+
+char_cols_to_check <- Filter(length, char_cols_to_check)
+
+# Checked all nonignorable character columns identified in following tables. Only
+# "ImageryPrime$situation" contains potential identifiable info.
+
+checked_tables <- names(char_cols_to_check)
+
+all(checked_tables == c("AnxietyTriggers", "CIHS", "DD_FU", "Demographics", 
+                        "ImageryPrime", "MentalHealthHxTx", "MultiUserExperience", 
+                        "ReturnIntention", "TrialDAO"))
+
+# ---------------------------------------------------------------------------- #
+# Redact "ImageryPrime$situation" ----
+# ---------------------------------------------------------------------------- #
+
+# Redact descriptions of an anxious situation for anxiety imagery prime task
+
+dat$ImageryPrime$situation <- "REDACTED_BY_CLEANING_SCRIPT"
+
+# ---------------------------------------------------------------------------- #
+# List tables that have been redacted ----
+# ---------------------------------------------------------------------------- #
+
+# List all tables that have been redacted by this script so that the redacted
+# files can be named appropriately when outputted
+
+redacted_tbls <- "ImageryPrime"
+
+# ---------------------------------------------------------------------------- #
+# Export redacted data ----
+# ---------------------------------------------------------------------------- #
+
+# Prepare filenames, preventing "-redacted" from being appended multiple times
+# if the script is run on any files that have already been redacted
+
+redacted_filename_stems <- sub("*.csv", "", 
+                               filenames[grep(paste0(redacted_tbls, collapse = "-|"), filenames)])
+
+redacted_filename_stems <- gsub("-redacted", "", redacted_filename_stems)
+
+redacted_filenames <- paste0(redacted_filename_stems, "_redacted.csv")
+
+# Write redacted CSV files. Remember not to share corresponding raw data files.
+
+redacted_data_dir <- "./data/2_redacted"
+dir.create(redacted_data_dir)
+
+dat_red <- dat[names(dat) %in% redacted_tbls]
+
+for (i in 1:length(dat_red)) {
+  write.csv(dat_red[[i]], 
+            paste0("./data/2_redacted/", redacted_filenames[i]),
+            row.names = FALSE)
+}
